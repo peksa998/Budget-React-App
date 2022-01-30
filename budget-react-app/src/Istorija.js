95e4d293579aeeed3fb2a0de36649a326a6e6cce
Dejan Petrovic <dejanp775@gmail.com>
2022-01-30 16:23:34 +0100

    v1.5
    neke izmene

--- budget-react-app/src/Istorija.js
+++ budget-react-app/src/Istorija.js
@@ -2,5 +2,5 @@ import React from "react";
 import { TransactionList } from "./components/TransactionList";
 
 export const Istorija = () => {
-  return <TransactionList title="Hello" />;
+  return <TransactionList />;
 };
